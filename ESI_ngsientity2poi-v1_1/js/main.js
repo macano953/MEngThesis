50740c208f3e90275e0fbefc581582cb7dd68176
Miguel Angel Caño Rojano <macano953@users.noreply.github.com>
2015-03-04 19:28:39 +0100

    Update main.js

--- ESI_ngsientity2poi-v1_1/js/main.js
+++ ESI_ngsientity2poi-v1_1/js/main.js
@@ -1,42 +1,3 @@
-/*
- *     Copyright (c) 2013-2014 CoNWeT Lab., Universidad Politécnica de Madrid
- *
- *     This file is part of the observation2poi operator.
- *
- *     observation2poi is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as published
- *     by the Free Software Foundation, either version 3 of the License, or (at
- *     your option) any later version.
- *
- *     observation2poi is distributed in the hope that it will be useful, but
- *     WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
- *     General Public License for more details.
- *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with observation2poi. If not, see <http://www.gnu.org/licenses/>.
- *
- *     Linking this library statically or dynamically with other modules is
- *     making a combined work based on this library.  Thus, the terms and
- *     conditions of the GNU Affero General Public License cover the whole
- *     combination.
- *
- *     As a special exception, the copyright holders of this library give you
- *     permission to link this library with independent modules to produce an
- *     executable, regardless of the license terms of these independent
- *     modules, and to copy and distribute the resulting executable under
- *     terms of your choice, provided that you also meet, for each linked
- *     independent module, the terms and conditions of the license of that
- *     module.  An independent module is a module which is not derived from
- *     or based on this library.  If you modify this library, you may extend
- *     this exception to your version of the library, but you are not
- *     obligated to do so.  If you do not wish to do so, delete this
- *     exception statement from your version.
- *
- */
-
-/*global MashupPlatform*/
-
 (function () {
 
     "use strict";
@@ -49,25 +10,12 @@
     MashupPlatform.wiring.registerCallback("entityInput", function (entityString) {
         var entity = JSON.parse(entityString);
         var coordinates = null;
-/*
-
-        var coordinates_attr = MashupPlatform.prefs.get('coordinates_attr');
-        if (entity[coordinates_attr]) {
-            var parts = entity[coordinates_attr].split(new RegExp(',\\s*'));
-            if (parts.length === 2) {
-                coordinates = {
-                    system: "WGS84",
-                    lat: parseFloat(parts[0]),
-                    lng: parseFloat(parts[1])
-                };
-            }
-        }
-	*/	
-		var coordinates_attr = MashupPlatform.prefs.get('coordinates_attr');
-		var parts = coordinates_attr.split(new RegExp(',\\s*'));
-		var latitude = entity[parts[0]];
-		var longitude = entity[parts[1]];
-		coordinates = {
+        
+	var coordinates_attr = MashupPlatform.prefs.get('coordinates_attr');
+	var parts = coordinates_attr.split(new RegExp(',\\s*'));
+	var latitude = entity[parts[0]];
+	var longitude = entity[parts[1]];
+	coordinates = {
 			system: "WGS84",
 			lat: parseFloat(latitude),
 			lng: parseFloat(longitude)
